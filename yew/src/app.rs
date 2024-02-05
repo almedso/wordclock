2d23b694aca2645fd6ad70423d4745b04237b6d9
Volker Kempert <volker.kempert@almedso.de>
2024-02-05 22:17:18 +0100

    Introduce interval for recurrent time update

--- yew/src/app.rs
+++ yew/src/app.rs
@@ -1,32 +1,76 @@
+use chrono::{Timelike, Utc};
+use gloo::timers::callback::Interval;
 use wordclock::WordClock;
 use yew::prelude::*;
-use chrono::{Utc, Timelike};
 
+pub enum Msg {
+    UpdateTime,
+}
 
-#[function_component(App)]
-pub fn app() -> Html {
+pub struct App {
+    _standalone: Interval,
+    language: String,
+    hour: usize,
+    minute: usize,
+}
 
-    let word_clock = WordClock::new("ch-bern".to_string());
-    let now = Utc::now();
+impl App {
+    fn get_current_time() -> (usize, usize) {
+        let now = Utc::now();
+        (now.hour() as usize, now.minute() as usize)
+    }
+}
 
-    let display_letters = word_clock
-        .show_time_iterator(now.hour() as usize, now.minute() as usize)
-        .map(|(letter, highlight, _end_of_row)| {
-            html! {
-                if highlight {
-                    <div style="color:green;">  { format!("{}", letter)  } </div>
-                } else {
-                    <div style="color:#333333;"> { format!("{}", letter) } </div>
-                }
+impl Component for App {
+    type Message = Msg;
+    type Properties = ();
+
+    fn create(ctx: &Context<Self>) -> Self {
+        let _standalone = {
+            let link = ctx.link().clone();
+            Interval::new(1000, move || link.send_message(Msg::UpdateTime))
+        };
+
+        let (hour, minute) = App::get_current_time();
+        Self {
+            _standalone,
+            hour,
+            minute,
+            language: "ch-bern".to_string(),
+        }
+    }
+
+    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
+        match msg {
+            Msg::UpdateTime => {
+                (self.hour, self.minute) = App::get_current_time();
+                true
             }
-        })
-        .collect::<Html>();
+        }
+    }
 
-    html! {
-        <main>
-            <div class="grid-container">
-                {display_letters}
-            </div>
-        </main>
+    fn view(&self, _ctx: &Context<Self>) -> Html {
+
+        let word_clock = WordClock::new(self.language.clone());
+        let display_letters = word_clock
+            .show_time_iterator(self.hour, self.minute)
+            .map(|(letter, highlight, _end_of_row)| {
+                html! {
+                    if highlight {
+                        <div style="color:green;">  { format!("{}", letter)  } </div>
+                    } else {
+                        <div style="color:#333333;"> { format!("{}", letter) } </div>
+                    }
+                }
+            })
+            .collect::<Html>();
+
+        html! {
+            <main>
+                <div class="grid-container">
+                    {display_letters}
+                </div>
+            </main>
+        }
     }
 }
